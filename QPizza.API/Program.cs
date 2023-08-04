8173c330164f406ec7eff0427a587c0d927882a4
Maxim Saraev <masaraev@yandex.ru>
2023-08-05 00:51:45 +0300

    auth controller foundation

--- QPizza.API/Program.cs
+++ QPizza.API/Program.cs
@@ -1,4 +1,8 @@
 
+using QPizza.Application;
+using QPizza.Application.Services.Authentication;
+using QPizza.Infrastructure;
+
 namespace QPizza.API
 {
     public class Program
@@ -7,16 +11,16 @@ namespace QPizza.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services
+                .AddApplication()
+                .AddInfrastructure();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
---
+++ QPizza.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QPizza.Application.Services.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QPizza.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            return services;
+        }
+    }
+}
